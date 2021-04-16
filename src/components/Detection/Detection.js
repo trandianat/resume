1ddfc1ddb10849df98acbc980f95d11229706081
Diana Tran <trandianat@gmail.com>
2021-04-15 20:49:58 -0400

    Added detection images

--- src/components/Detection/Detection.js
+++ src/components/Detection/Detection.js
@@ -2,48 +2,47 @@ import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import '@tensorflow/tfjs-backend-cpu';
 import '@tensorflow/tfjs-backend-webgl';
-import '@tensorflow/tfjs-converter';
-import '@tensorflow/tfjs-core';
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import Button from 'components/Button';
 import Title from 'components/Title';
-import cars from './images/cars.jpeg';
 import dog from './images/dog.jpg';
+import street from './images/street.jpg';
+import table from './images/table.jpg';
+import wine from './images/wine.jpg';
 import './Detection.scss';
 
 const Detection = () => {
-    const [image, setImage] = useState(cars);
+    const [image, setImage] = useState(dog);
     const [loading, setLoading] = useState(false);
     const [results, setResults] = useState([]);
     const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const selectedImage = document.getElementById('detection-image');
 
     useEffect(() => {
-        const img = document.getElementById('detection-image');
         const canvas = document.getElementById('canvas');
         const context = canvas.getContext('2d');
         context.clearRect(0, 0, canvas.width, canvas.height);
         if (results.length > 0) {
-            canvas.style.border = 'none';
-            context.drawImage(img, 0, 0, img.offsetWidth, img.offsetHeight);
-            results.forEach(result => {
+            const colors = ['red', 'blue', 'green', 'orange', 'purple', 'gray', 'pink', 'brown', 'black', 'yellow'];
+            context.drawImage(selectedImage, 0, 0, selectedImage.offsetWidth, selectedImage.offsetHeight);
+            results.forEach((result, index) => {
                 context.beginPath();
                 context.lineWidth = '2';
-                context.strokeStyle = 'red';
+                context.strokeStyle = colors[index];
                 const [ x, y, width, height ] = result.bbox;
                 context.strokeRect(x, y, width, height);
             });
         } else {
-            canvas.style.border = '1px solid gray';
             context.font = '14px Arial';
             context.textAlign = 'center';
             context.fillText('Results will be shown here', canvas.width / 2, canvas.height / 2);
         }
-    }, [results]);
+    }, [results, selectedImage]);
 
     const detectImage = async () => {
         setLoading(true);
         const model = await cocoSsd.load();
-        const predictions = await model.detect(document.getElementById('detection-image'));
+        const predictions = await model.detect(selectedImage, 10, .25);
         setResults(predictions);
         setLoading(false);
     };
@@ -56,11 +55,13 @@ const Detection = () => {
                 setImage(event.target.value);
                 setResults([]);
             }}>
-                <option value={cars}>cars</option>
                 <option value={dog}>dog</option>
+                <option value={street}>street</option>
+                <option value={table}>table</option>
+                <option value={wine}>wine</option>
             </select>
             <img alt="Detection test" id="detection-image" src={image} />
-            <canvas height={document.getElementById('detection-image')?.offsetHeight} id="canvas" />
+            <canvas height={selectedImage?.offsetHeight} id="canvas" width={selectedImage?.offsetWidth} />
             {results.length > 0 && results.map(result => (
                 <p>{`A${[vowels].includes(result.class.charAt(0)) ? 'n' : ''} ${result.class} with ${Math.round(result.score * 100)}% confidence`}</p>
             ))}
@@ -70,7 +71,7 @@ const Detection = () => {
                     label={loading ? 'Loading...' : 'Detect image'}
                     onClick={() => detectImage()}
                     variant="primary"
-                    {...({ disabled: loading })}
+                    {...{ disabled: loading }}
                 />
             }
         </div>
